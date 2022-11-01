3e3773c59048394ebfd00bd13f50ba8feac16f7f
PedroSehn <pedrorsehn@hotmail.com>
2022-11-01 15:06:18 -0300

    add ex19-20-21

--- app/backend/src/controllers/Match.Controller.ts
+++ app/backend/src/controllers/Match.Controller.ts
@@ -5,6 +5,16 @@ class MatchController {
   private _service = new MatchesService();
 
   public getAllMatches = async (rec: Request, res: Response) => {
+    if (rec.query.inProgress) {
+      const { query } = rec;
+      const valid = query.inProgress;
+      if (valid === 'true') {
+        const data = await this._service.getAllInProgress();
+        return res.status(200).json(data);
+      }
+      const result = await this._service.getAllFinished();
+      return res.status(200).json(result);
+    }
     const response = await this._service.getAllMatches();
     return res.status(200).json(response);
   };
--- app/backend/src/database/services/Matches.service.ts
+++ app/backend/src/database/services/Matches.service.ts
@@ -13,6 +13,28 @@ class MatchesService {
     });
     return result;
   };
+
+  public getAllFinished = async () => {
+    const result = await this._model.findAll({
+      where: { inProgress: 0 },
+      include: [
+        { model: Team, as: 'teamHome', attributes: { exclude: ['id'] } },
+        { model: Team, as: 'teamAway', attributes: { exclude: ['id'] } },
+      ],
+    });
+    return result;
+  };
+
+  public getAllInProgress = async () => {
+    const result = await this._model.findAll({
+      where: { inProgress: 1 },
+      include: [
+        { model: Team, as: 'teamHome', attributes: { exclude: ['id'] } },
+        { model: Team, as: 'teamAway', attributes: { exclude: ['id'] } },
+      ],
+    });
+    return result;
+  };
 }
 
 export default MatchesService;
